557225fda7e33c266afad1d099bcbf2784dea562
rivash <rivijerrier@gmail.com>
2022-12-14 10:33:24 -0800

    creates a file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # creates a file
 
-file {'/tmp/school':
-	ensure => 'file',
-	path => '/tmp/school',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet'
+file { '0-create_a_file':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
