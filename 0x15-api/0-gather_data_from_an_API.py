34cbf8baed1c5dc88fef5631cf69662e06021665
Rivashjerrier <rivijerrier@gmail.com>
2023-01-16 21:43:48 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-""" Returns emp TODO list progress using REST API """
+""" 
+Returns information about employee TODO list progress
+using REST API for a given employee ID
+"""
 
 import requests as req
 from sys import argv
 
 if __name__ == "__main__":
+
+    """ Gets employee information """
     emp = req.get("https://jsonplaceholder.typicode.com/users" +
 		  argv[1]).json()
     tasks = req.get("https://jsonplaceholder.typicode.com/todos?userId=" +
@@ -12,6 +17,7 @@ if __name__ == "__main__":
     tasks_done = req.get("https://jsonplaceholder.typicode.com/todos?userId=" +
 			 argv[1] + "&completed=true").json
 
+    """ Prints employee information """
     print("Employee {} is done with task({}/{}):"
 	  .format(emp.get("name"), len(tasks_done), len(tasks)))
     for task in tasks_done:
