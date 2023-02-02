449cf26afe31ec4e042d70387bafff1e778fef96
Rivashjerrier <rivijerrier@gmail.com>
2023-02-02 21:04:36 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Gets the number of subscribers for a given subreddit"""
+    """
+    Gets the number of subscribers for a given subreddit
+    """
     api = "http://reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(api, headers=headers, allow_redirects=False)
