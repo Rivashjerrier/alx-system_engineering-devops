7f89bd23904786e5a8035b9390325b3eebc97ea3
Rivashjerrier <rivijerrier@gmail.com>
2023-02-02 20:28:47 +0000

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number
+of subscribers for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Gets the number of subscribers for a given subreddit
+    """
+    api = "http://reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(api, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    subscribers = response.json().get("data")
+    return subscribers.get("subscribers"
