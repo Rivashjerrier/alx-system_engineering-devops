d3a5334d2ae7a075a11bfb9ef367a6cd76b5b407
Rivashjerrier <rivijerrier@gmail.com>
2023-02-03 02:40:09 +0000

    Count it

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""recursive function queries Reddit API, parses the title of hot articles"""
+import requests
+
+
+def count_words(subreddit, word_list)
+    """parses the title of all hot articles, and prints a sorted count"""
