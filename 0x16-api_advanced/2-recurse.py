7390bf5dab74cf73da7ae56d17ed7d201f47536c
Rivashjerrier <rivijerrier@gmail.com>
2023-02-03 00:08:47 +0000

    task3

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-recursive function that queries the Reddit API and returns a list containing
-the titles of all hot articles for a given subreddit
+2-recurse - recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit
 """
 import requests
 import sys
