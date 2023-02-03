0afed083c53175c0b6d162abfdf75168cf52120f
Rivashjerrier <rivijerrier@gmail.com>
2023-02-03 01:27:43 +0000

    devops

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""0-subs - Queries the Reddit API and returns the number of subscribers for a given subreddit"""
+"""Queries the Reddit API and returns the number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Gets the number of subscribers for a given subreddit
-    """
-
+    """Gets the number of subscribers for a given subreddit"""
     api = "http://reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(api, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         subscribers = response.json()
-        """Number of subscribers"""
         num = subscribers.get('data').get('subscribers')
         return num
    else:
