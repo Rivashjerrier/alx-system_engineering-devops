5bc32927da83ae1020be64a66fcb07561def6c87
Rivashjerrier <rivijerrier@gmail.com>
2023-02-03 01:35:08 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Gets the number of subscribers for a given subreddit"""
-    api = "http://reddit.com/r/{}/about.json".format(subreddit)
+    url = "http://reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(api, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        subscribers = response.json()
-        num = subscribers.get('data').get('subscribers')
-        return num
-   else:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+        subscribers = response.json().get("data")
+        return subscribers.get("subscribers")
