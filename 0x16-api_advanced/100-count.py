ca1697762fa0e6d4aaef40695a775bbfb8184d59
Rivashjerrier <rivijerrier@gmail.com>
2023-03-16 14:25:52 +0000

    Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,26 +4,20 @@ import requests
 
 
 def count_words(subreddit, word_list, found_list=[], after=None):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        found_list (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-    """
-    user_agent = {'User-agent': 'test45'}
-    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+    """Prints counts of given words found in hot posts"""
+    user_agent = {"User-agent": "Mozilla/5.0"}
+    posts = requests.get("http://www.reddit.com/r/{}/hot.json?after={}"
                          .format(subreddit, after), headers=user_agent)
     if after is None:
         word_list = [word.lower() for word in word_list]
 
     if posts.status_code == 200:
-        posts = posts.json()['data']
-        aft = posts['after']
-        posts = posts['children']
+        posts = posts.json()["data"]
+        aft = posts["after"]
+        posts = posts["children"]
         for post in posts:
-            title = post['data']['title'].lower()
-            for word in title.split(' '):
+            title = post["data"]["title"].lower()
+            for word in title.split(" "):
                 if word in word_list:
                     found_list.append(word)
         if aft is not None:
@@ -37,6 +31,6 @@ def count_words(subreddit, word_list, found_list=[], after=None):
                     result[word.lower()] = 1
             for key, value in sorted(result.items(), key=lambda item: item[1],
                                      reverse=True):
-                print('{}: {}'.format(key, value))
+                print("{}: {}".format(key, value))
     else:
         return
